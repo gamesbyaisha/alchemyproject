510b90af077ce68aedfce83433418199f1a0b3a4
gamesbyaisha <nekoh150@newschool.edu>
2022-11-17 23:55:18 -0500

    added continue text

--- Alchemy/Assets/Scripts/TextInstructions.cs
+++ Alchemy/Assets/Scripts/TextInstructions.cs
@@ -7,11 +7,12 @@ public class TextInstructions : MonoBehaviour
 {
     public TMP_Text instructionsText;
     public Image grey;
+    public TMP_Text continueText;
  
      // Use this for initialization
     void Start()
         {
-            instructionsText.text = "Please help... I am dying";
+            instructionsText.text = "Please help... I am dying... I am 239 years old and I have been practicing witchcraft my whole life...";
             Time.timeScale = 0;
             grey.enabled = true;
         }
@@ -20,7 +21,8 @@ public class TextInstructions : MonoBehaviour
         {
             if(Input.anyKeyDown)
             {
-                instructionsText.text = "I need to collect all the potions to become young again... Can you help me? Press space";
+                instructionsText.text = "I need to collect all the potions to become young again... Can you help me?";
+                continueText.enabled = true;
             }
         }
          {
