dc5a37672ef6f937d64703ef3b1ac6490bbf686a
gamesbyaisha <nekoh150@newschool.edu>
2022-11-22 23:09:09 -0500

    changed the background to another drawing

--- Alchemy/Assets/Scripts/GoalManager.cs
+++ Alchemy/Assets/Scripts/GoalManager.cs
@@ -1,43 +1,43 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
 
-[System.Serializable]
-public class BlankGoal
-{
-    public int numberNeeded;
-    public int numberCollected;
-    public Sprite goalSprite;
-    public string matchValue;
-}
+// [System.Serializable]
+// public class BlankGoal
+// {
+//     public int numberNeeded;
+//     public int numberCollected;
+//     public Sprite goalSprite;
+//     public string matchValue;
+// }
 
-public class GoalManager : MonoBehaviour
-{
-    public BlankGoal[] levelGoals;
-    public GameObject goalPrefab;
-    public GameObject goalIntroParent;
-    public GameObject goalGameParent;
-    // Start is called before the first frame update
-    void Start()
-    {
-        SetupIntroGoals();
-    }
+// public class GoalManager : MonoBehaviour
+// {
+//     public BlankGoal[] levelGoals;
+//     public GameObject goalPrefab;
+//     public GameObject goalIntroParent;
+//     public GameObject goalGameParent;
+//     // Start is called before the first frame update
+//     void Start()
+//     {
+//         SetupIntroGoals();
+//     }
 
-    void SetupIntroGoals()
-    {
-        for(int i = 0; i < levelGoals.Length; i++)
-        {
-            //create new goal panel
-            GameObject goal = Instantiate(goalPrefab, goalIntroParent.transform.position, Quaternion.identity);
-            goal.transform.SetParent(goalIntroParent.transform);
-            GameObject gameGoal = Instantiate(goalPrefab, goalGameParent.transform.position, Quaternion.identity);
-            goal.transform.SetParent(goalGameParent.transform);
-        }
-    }
+//     void SetupIntroGoals()
+//     {
+//         for(int i = 0; i < levelGoals.Length; i++)
+//         {
+//             //create new goal panel
+//             GameObject goal = Instantiate(goalPrefab, goalIntroParent.transform.position, Quaternion.identity);
+//             goal.transform.SetParent(goalIntroParent.transform);
+//             GameObject gameGoal = Instantiate(goalPrefab, goalGameParent.transform.position, Quaternion.identity);
+//             goal.transform.SetParent(goalGameParent.transform);
+//         }
+//     }
 
-    // Update is called once per frame
-    void Update()
-    {
+//     // Update is called once per frame
+//     void Update()
+//     {
         
-    }
-}
+//     }
+// }
